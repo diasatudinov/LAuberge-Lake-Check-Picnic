03c414f84015b9c5c15c6a9e9e83e7948262a584
Dias <dias.atudinov@gmail.com>
2026-02-11 21:07:59 +0500

    new files

---
+++ LAuberge Lake Check Picnic/LAuberge Lake Check Picnic/LLModules/LLType/LLNewTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  LLNewTypeView.swift
+//  LAuberge Lake Check Picnic
+//
+//  Created by Dias Atudinov on 11.02.2026.
+//
+
+import SwiftUI
+
+struct LLNewTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LLNewTypeView()
+}
